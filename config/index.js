dd81d514a24f89342a68ea7630cb4f596b6bf826
Yoann Picquenot <yoann.picquenot@epitech.eu>
2017-05-04 15:40:32 -0400

    Added four indentation spaces in files and remove console log

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  server: {
-    port: process.env.PORT || 5000
-  },
-  mongodb_uri: process.env.MONGODB_URI
+    server: {
+        port: process.env.PORT || 5000
+    },
+    mongodb_uri: process.env.MONGODB_URI
 };
