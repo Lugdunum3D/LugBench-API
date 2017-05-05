4bd0331a5bcc68b8a86f3a17e42b69f00cb1cb2e
Yoann Picquenot <yoann.picquenot@epitech.eu>
2017-05-05 17:01:27 -0400

    Modified test, and removed useless gpuErrorTest file

--- test/add-gpu.js
+++ test/add-gpu.js
@@ -2,35 +2,34 @@ var assert = require('assert');
 var mongoose = require('mongoose');
 var config = require('../config');
 var Gpu = require('../v1/models/gpu');
-
 var gpuTest = require('./gputest.js');
-var gpuErrorTest = require('./gpuErrorTest.js');
 
-describe('Addition of gpu to database', () => {
-  describe('Connection to database', () => {
-    it('should connect to database', (done) => {
-      mongoose.Promise = global.Promise;
-      mongoose.connect(config.mongodb_uri, done);
+mongoose.connect(config.mongodb_uri);
+
+describe('Gpu', () => {
+    beforeEach((done) => {
+        Gpu.remove({}, done);
     });
-  });
 
-  describe('Add object json', () => {
-    it('should failed to create', (done) => {
-      let gpu = new Gpu(gpuErrorTest);
-
-      gpu.save().then((doc) => {
-        assert('Succeed', 'That\'s not normal.')
-      }, (err) => {
-        done();
-      });
+    afterEach((done) => {
+        Gpu.remove({}, done);
     });
 
     it('should add to database', (done) => {
-      let gpu = new Gpu(gpuTest);
+        let gpu = new Gpu(gpuTest);
 
-      gpu.save().then((doc) => {
-        done();
-      });
+        gpu.save().then((doc) => {
+            done();
+        }, done);
     });
-  });
+
+    it('should not add to database', (done) => {
+        gpuTest.formats = 'error';
+        let gpu = new Gpu(gpuTest);
+
+        gpu.save().then(done, () => {
+            done();
+        });
+    })
+
 });
