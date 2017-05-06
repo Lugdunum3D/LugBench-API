d9f21d75e9d62688d4de56fa2dc55d3949dfa386
Yoann Picquenot <yoann.picquenot@epitech.eu>
2017-05-06 15:37:01 -0400

    Removed warning due to mongoose promises & Add conditions

--- v1/index.js
+++ v1/index.js
@@ -7,20 +7,28 @@ var Gpu = require('./models/gpu');
 var router = express.Router();
 
 router.put('/gpus', (req, res) => {
-    var newGpu = new Gpu(req.body);
-    newGpu.save().then((doc) => {
-        res.status(200).send({
-            data: doc
+    if (!req.body || !req.body.length) {
+        res.status(400).send('Missing object');
+    } else {
+        let newGpu = new Gpu(req.body);
+        let promise = newGpu.save();
+
+        promise.then((doc) => {
+            res.status(200).send({
+                data: doc
+            });
+        }, (err) => {
+            res.status(500).send({
+                err: err
+            });
         });
-    }, (err) => {
-        res.status(500).send({
-            err: err
-        });
-    });
+    }
+
 });
 
 router.get('/gpus', (req, res) => {
-    Gpu.find().then((doc) => {
+    let promise = Gpu.find().exec();
+    promise.then((doc) => {
         res.status(200).send({
             data: doc
         });
@@ -30,13 +38,17 @@ router.get('/gpus', (req, res) => {
 router.get('/gpus/:id', (req, res) => {
     let id = req.params.id;
 
-    Gpu.findOne({
-        '_id': mongoose.Types.ObjectId(id)
-    }).then((doc) => {
-        res.status(200).send({
-            data: doc
+    if (!id) {
+        res.status(400).send('Missing id parameters');
+    } else {
+        Gpu.findOne({
+            '_id': mongoose.Types.ObjectId(id)
+        }).exec().then((doc) => {
+            res.status(200).send({
+                data: doc
+            });
         });
-    });
+    }
 });
 
 module.exports = router;
