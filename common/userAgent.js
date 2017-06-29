0150c7b05d45932c50472160c8a2273a81a22ff9
cchardeau <corentin.chardeau@epitech.eu>
2017-06-29 05:14:17 +0200

    Fix parse function in UserAgent

--- common/userAgent.js
+++ common/userAgent.js
@@ -13,16 +13,16 @@ Agent.prototype.version = function version() {
     return this.major + '.' + this.minor + '.' + this.patch
 }
 
- function parse(userAgent){
-    var userAgentSplited = userAgent.split("/")
-    if (userAgentSplited.length !== 3 || userAgentSplited[0] !== "LugBench" ){
+ function parse(userAgent) {
+    const userAgentSplited = userAgent.split("/")
+    if (userAgentSplited.length !== 2 || userAgentSplited[0] !== "LugBench" ){
         return new Agent()
     }
-    var family = userAgentSplited[0]
-    var source = userAgentSplited[2]
-    var versionSplitted = userAgentSplited[1].split('.')
+    const family = userAgentSplited[0]
+    const source = userAgentSplited[2]
+    const versionSplitted = userAgentSplited[1].split('.')
     if (versionSplitted.length !== 3) {
-         return new Agent()  
+         return new Agent()
     }
     return new Agent(family, versionSplitted[0], versionSplitted[1],versionSplitted[2], source)
 }
@@ -31,8 +31,7 @@ module.exports.parse = parse
 module.exports.Agent = Agent
 
 
-//   to-do : 
-//    to Json function 
+//   to-do :
+//    to Json function
  //   to string Versiom
  // function parse string to agent
-
