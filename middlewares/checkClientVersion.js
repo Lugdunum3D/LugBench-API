bc7ccfcdab1ba54b3cbe54d175ec62154f5c3240
cchardeau <corentin.chardeau@epitech.eu>
2017-06-29 05:02:57 +0200

    Fix errors in checkClientVersion

--- middlewares/checkClientVersion.js
+++ middlewares/checkClientVersion.js
@@ -1,15 +1,16 @@
 "use strict"
 
+const errors    = require('restify-errors')
 const config    = require('../config')
 const UserAgent = require('../common/userAgent')
 
 module.exports.checkClientVersion = function(req, res, next) {
     if (!req.headers['user-agent']) {
-        return res.send(401)
+        return next(new errors.UnauthorizedError('Bad user-agent'))
     }
     var ua = UserAgent.parse(req.headers['user-agent'])
     if (ua.family !== "LugBench"  || ua.version() !== config.client_version){
-        return res.send(401)
+        return next(new errors.UnauthorizedError('Bad user-agent'))
     }
     next()
 }
