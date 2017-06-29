af70b0c87b7a79de7658759a427cefc081968ff5
cchardeau <corentin.chardeau@epitech.eu>
2017-06-29 06:00:18 +0200

    Basic tests of devices

--- test/device.spec.js
+++ test/device.spec.js
@@ -1,21 +1,78 @@
 const server    = require('../index')
 const chai      = require('chai')
 const supertest = require('supertest')
+const mongoose  = require('mongoose')
 
 const expect    = chai.expect;
 const app       = supertest.agent('http://localhost:5000')
+const db        = mongoose.connection
 
-const device    = require('../assets/mocks/devices/530.json')
+const device530 = require('../assets/mocks/devices/530.json')
+const device960M = require('../assets/mocks/devices/960M.json')
+
+before(function (done) {
+  db.collection('devices').drop(function () {
+    done()
+  })
+});
+
+beforeEach(function (done) {
+  done()
+})
+
+afterEach(function (done) {
+  db.collection('devices').drop(function () {
+    done()
+  })
+});
 
 describe('Device', function() {
 
-  describe('GET', function() {
-    it('should return an empty list of devices', function(done) {
+  describe('GET /devices', function() {
+    it('should return a list of devices', function(done) {
+      db.collection('devices').insert(device530, function(err, first) {
+        db.collection('devices').insert(device960M, function(err, second) {
+          app
+          .get('/devices')
+          .end(function(err, res) {
+            expect(res.body.data.length).to.be.equal(2)
+            expect(res.body.data[0]._id).to.have.string(first.ops[0]._id)
+            expect(res.body.data[1]._id).to.have.string(second.ops[0]._id)
+            expect(res.body.data[0].name).to.be.equal(first.ops[0].name)
+            expect(res.body.data[1].name).to.be.equal(second.ops[0].name)
+            expect(res.statusCode).to.be.equal(200)
+            done()
+          })
+        })
+      })
+    })
+  })
+
+  describe('GET /devices/:id', function() {
+    it('should return one device', function(done) {
+      db.collection('devices').insert(device530, function(err, device) {
+        app
+        .get('/devices/' + device.ops[0]._id)
+        .end(function(err, res) {
+          expect(res.body.length).to.be.equal(1)
+          expect(res.body[0]._id).to.have.string(device.ops[0]._id)
+          expect(res.body[0].name).to.be.equal(device.ops[0].name)
+          expect(res.statusCode).to.be.equal(200)
+          done()
+        })
+      })
+    })
+  })
+
+  describe('POST /devices', function() {
+    it('should create a device', function(done) {
       app
-      .get('/devices')
+      .post('/devices')
+      .send(device530)
+      .set('User-Agent', 'LugBench/0.1.0')
       .end(function(err, res) {
-        expect(res.body).to.be.empty;
-        expect(res.statusCode).to.be.equal(200)
+        expect(res.body._id).to.not.be.null
+        expect(res.statusCode).to.be.equal(201)
         done()
       })
     })
