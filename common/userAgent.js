bc21e6e819e3fc175c3cd24340db7e7e43723209
Guillaume <guillaume-heritiana.sabatie@epitech.eu>
2017-07-06 12:05:32 +0200

    added latest version of classs Agent

--- common/userAgent.js
+++ common/userAgent.js
@@ -1,35 +1,45 @@
-'use strict'
+'use strict';
 
-function Agent(family, major, minor, patch, source) {
-    this.family = family || 'Other'
-    this.major = major || '0'
-    this.minor = minor || '0'
-    this.patch = patch || '0'
-    this.source = source || 'other'
+ /**
+  * @param  {String} family This is the Client Name for us Lugbench
+  * @param  {Int} major  This is the major number of client version
+  * @param  {Int} minor  This is the minor number of client version
+  * @param  {Int} patch  This is the patch number of client version
+  * @param  {String} source This is the operating system from the client
+  */
+ function Agent(family, major, minor, patch, source) {
+  this.family = family || 'Other';
+  this.major = major || 0;
+  this.minor = minor || 0;
+  this.patch = patch || 0;
+  this.source = source || 'other';
 }
 
 Agent.prototype.version = function version() {
-    return this.major + '.' + this.minor + '.' + this.patch
-}
+    return this.major + '.' + this.minor + '.' + this.patch;
+};
 
-function parse(userAgent) {
-    const userAgentSplited = userAgent.split('/')
-    if (userAgentSplited.length !== 2 || userAgentSplited[0] !== 'LugBench') {
-        return new Agent()
+/**
+ * Transform a String to a Agent Object
+ * @param {String} userAgent This is the string to parse
+ * @return {Agent} This return an Agent object
+ */
+ function parse(userAgent) {
+    let userAgentSplited = userAgent.split('/');
+    if (userAgentSplited.length !== 3 || userAgentSplited[0] !== 'LugBench' ) {
+        return new Agent();
     }
-    const family = userAgentSplited[0]
-    const source = userAgentSplited[2]
-    const versionSplitted = userAgentSplited[1].split('.')
+    let family = userAgentSplited[0];
+    let source = userAgentSplited[2];
+    let versionSplitted = userAgentSplited[1].split('.');
     if (versionSplitted.length !== 3) {
-        return new Agent()
+         return new Agent();
     }
-    return new Agent(family, versionSplitted[0], versionSplitted[1],versionSplitted[2], source)
+    return new Agent(family,
+      versionSplitted[0],
+      versionSplitted[1],
+      versionSplitted[2],
+      source);
 }
-
-module.exports.parse = parse
-module.exports.Agent = Agent
-
-//   to-do :
-//    to Json function
-//   to string Versiom
-// function parse string to agent
+module.exports.parse = parse;
+module.exports.Agent = Agent;
