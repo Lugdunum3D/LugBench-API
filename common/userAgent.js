b5d05d6d17d38ab026ba5d040b7d6279a55c7385
Guillaume <guillaume-heritiana.sabatie@epitech.eu>
2017-07-06 12:12:31 +0200

    fixed source is now the original user agent

--- common/userAgent.js
+++ common/userAgent.js
@@ -5,14 +5,14 @@
   * @param  {Int} major  This is the major number of client version
   * @param  {Int} minor  This is the minor number of client version
   * @param  {Int} patch  This is the patch number of client version
-  * @param  {String} source This is the operating system from the client
+  * @param  {String} source This is the actual user agent string
   */
  function Agent(family, major, minor, patch, source) {
-  this.family = family || 'Other';
+  this.family = family || '';
   this.major = major || 0;
   this.minor = minor || 0;
   this.patch = patch || 0;
-  this.source = source || 'other';
+  this.source = source || '';
 }
 
 Agent.prototype.version = function version() {
@@ -26,20 +26,20 @@ Agent.prototype.version = function version() {
  */
  function parse(userAgent) {
     let userAgentSplited = userAgent.split('/');
-    if (userAgentSplited.length !== 3 || userAgentSplited[0] !== 'LugBench' ) {
-        return new Agent();
+    if (userAgentSplited.length !== 2 || userAgentSplited[0] !== 'LugBench' ) {
+        return new Agent('', 0, 0, 0, userAgent);
     }
     let family = userAgentSplited[0];
     let source = userAgentSplited[2];
     let versionSplitted = userAgentSplited[1].split('.');
     if (versionSplitted.length !== 3) {
-         return new Agent();
+         return new Agent('', 0, 0, 0, userAgent);
     }
     return new Agent(family,
       versionSplitted[0],
       versionSplitted[1],
       versionSplitted[2],
-      source);
+      userAgent);
 }
 module.exports.parse = parse;
 module.exports.Agent = Agent;
