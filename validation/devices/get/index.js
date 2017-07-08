b81301d5fe799dd57027c3bfd849af65fd57985c
cchardeau <corentin.chardeau@epitech.eu>
2017-07-08 20:24:37 +0200

    Allow string or array for search a device by os

--- validation/devices/get/index.js
+++ validation/devices/get/index.js
@@ -5,7 +5,10 @@ const Joi = require('joi')
 module.exports = {
     'params': Joi.object().keys({
         'name': Joi.string(),
-        'os': Joi.string().valid('Windows', 'Linux', 'Android'),
+        'os':  [
+            Joi.string().valid('Windows', 'Linux', 'Android'),
+            Joi.array().items(Joi.string().valid('Windows', 'Linux', 'Android')),
+        ],
         'driverVersion': Joi.number(),
         'vendorId': Joi.number(),
         'deviceId': Joi.number(),
